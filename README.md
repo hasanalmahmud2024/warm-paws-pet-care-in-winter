ae867530fa85123df0e766b0f7fd44f9528b9ee1
Fallacy Falafel <noiraxen@proton.me>
2025-11-25 19:08:33 +0600

    basic project setup with react router

---
+++ README.md
@@ -0,0 +1,5 @@
+WarmPaws – Pet Care in Winter
+
+### Theme
+A cozy winter companion platform designed for pet owners to ensure their furry friends stay warm, safe, and healthy during the cold season. Users can explore local pet care services, winter pet clothing, grooming options, and expert tips — all in one friendly interface.
+
---
+++ src/Router/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import RootLayout from "../Layout/RootLayout";
+import Home from "../components/Home/Home";
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: RootLayout ,
+        children: [
+            {
+                index: true,
+                Component: Home,
+            }
+        ]
+    }
+])
+
+export default router,
