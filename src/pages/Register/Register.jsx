38ff4741b296994cf8896e851966d0dd74972bdd
Fallacy Falafel <noiraxen@proton.me>
2025-11-26 17:35:12 +0600

    react-hot-toast and password pattern check funcionality added:

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,25 +1,45 @@
-import React, { use } from 'react';
-import { Link } from 'react-router';
+import React, { use, useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router';
 import { AuthContext } from '../../context/AuthContext/AuthContext';
+import toast from 'react-hot-toast';
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io';
 
 const Register = () => {
+    const [showPassword, setShowPassword] = useState(false);
     const { createUser } = use(AuthContext);
+
+    const location = useLocation();
+    const navigate = useNavigate();
 
     const handleSignUp = (event) => {
         event.preventDefault()
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password);
-        
+
+        const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+        if (passwordPattern.test(password)) {
+            toast.error('Password must have uppercase, lowercase, and at least 6 characters');
+            return;
+        }
 
         createUser(email, password)
             .then(result => {
                 console.log(result.user);
+                toast.success('Account Registration successful');
+                setTimeout(() => {
+                    navigate(location?.state || '/');
+                }, 1000);
             })
             .catch((error => {
                 console.log(error)
+                toast.error(error.message)
             }))
+    }
+
+    const handleShowPassword = (event) => {
+        event.preventDefault();
+        setShowPassword(!showPassword);
     }
 
     return (
@@ -43,14 +63,21 @@ const Register = () => {
                                 <input name='photoURL' type="text" className="input" placeholder="PhotoURL" />
                                 {/* password */}
                                 <label className="label">Password</label>
-                                <input name='password' type="password" className="input" placeholder="Password" />
+                                <div className='relative'>
+                                    <button onClick={handleShowPassword} className="btn btn-xs absolute top-2 right-5 z-50">
+                                        {
+                                            showPassword ? <IoMdEyeOff></IoMdEyeOff> : <IoMdEye></IoMdEye>
+                                        }
+                                    </button>
+                                    <input name='password' type={showPassword ? 'text' : "password"} className="input pr-10" placeholder="Password" />
+                                </div>
                                 <button className="btn btn-neutral mt-4">Register</button>
                                 {/* Google */}
                                 <button className="btn bg-white text-black my-1">
                                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
-                                    SignIn with Google
+                                    SignUp with Google
                                 </button>
-                                <p>Already have an account? <Link to={'/login'} className='text-blue-400 link link-hover hover:text-blue-300'>Login</Link></p>
+                                <p className='text-sm'>Already have an account? <Link to={'/login'} className='text-blue-400 link link-hover hover:text-blue-300'>Login</Link></p>
                             </fieldset>
                         </form>
                     </div>
