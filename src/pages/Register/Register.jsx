7940ad8a7be91e7daa3fa061ec22ffc5e131565d
Fallacy Falafel <noiraxen@proton.me>
2025-11-26 20:39:06 +0600

    register form google sign up issue updated

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -8,7 +8,7 @@ import { auth } from '../../firebase/firebase.config';
 
 const Register = () => {
     const [showPassword, setShowPassword] = useState(false);
-    const { createUser, setUser } = use(AuthContext);
+    const { createUser, setUser, signInWithGoogle } = use(AuthContext);
 
     const location = useLocation();
     const navigate = useNavigate();
@@ -47,6 +47,16 @@ const Register = () => {
                 toast.error(error.message)
             }))
     }
+    const handleGoogleSignUp = () => {
+        signInWithGoogle()
+            .then(result => {
+                console.log(result.user);
+                navigate(location?.state || '/')
+            })
+            .catch((error => {
+                console.log(error)
+            }))
+    }
 
     const handleShowPassword = (event) => {
         event.preventDefault();
@@ -65,10 +75,10 @@ const Register = () => {
                             <fieldset className="fieldset">
                                 {/* name */}
                                 <label className="label">Your Name</label>
-                                <input name='name' type="text" className="input" required placeholder="Your Name" />
+                                <input name='name' type="text" className="input"  placeholder="Your Name" />
                                 {/* email */}
                                 <label className="label">Email</label>
-                                <input name='email' type="email" className="input" placeholder="Email" required />
+                                <input name='email' type="email" className="input" placeholder="Email"  />
                                 {/* photoURL */}
                                 <label className="label">Your Photo </label>
                                 <input name='photoURL' type="text" className="input" placeholder="PhotoURL" />
@@ -80,11 +90,11 @@ const Register = () => {
                                             showPassword ? <IoMdEyeOff></IoMdEyeOff> : <IoMdEye></IoMdEye>
                                         }
                                     </button>
-                                    <input name='password' type={showPassword ? 'text' : "password"} className="input pr-10" placeholder="Password" required />
+                                    <input name='password' type={showPassword ? 'text' : "password"} className="input pr-10" placeholder="Password"  />
                                 </div>
                                 <button className="btn btn-neutral mt-4">Register</button>
                                 {/* Google */}
-                                <button className="btn bg-white text-black my-1">
+                                <button onClick={handleGoogleSignUp} className="btn bg-white text-black my-1">
                                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
                                     SignUp with Google
                                 </button>
